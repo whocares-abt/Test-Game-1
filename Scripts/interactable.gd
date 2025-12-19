3ecdb3ccd0a5445cdd7a17380f8557e81636432a
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-12-20 02:38:34 +0530

    Added interactable NPC

---
+++ Scripts/interactable.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+# A class denoting if the given element has an interactable property
+@export var interact_text = ""
+@export var is_interactable = true
+
+# Defining a function
+var interact : Callable = func():
+	pass
