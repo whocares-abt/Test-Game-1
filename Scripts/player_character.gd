0a437a40271290896c4a00f1ed956039f4ba1fb4
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-12-20 01:26:28 +0530

    Created basic player character in graybox

---
+++ Scripts/player_character.gd
@@ -0,0 +1,41 @@
+extends CharacterBody2D
+
+@onready var sprite = $PlayerSprite
+
+@export var SPEED = 300
+
+func _physics_process(delta: float) -> void:
+
+	var direction_x = Input.get_axis("move_left", "move_right")
+	var direction_y = Input.get_axis("move_up", "move_down")
+
+	if direction_x:
+		velocity.x = direction_x * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction_y:
+		velocity.y = direction_y * SPEED
+	else:
+		velocity.y = move_toward(velocity.y, 0, SPEED)
+
+	play_animation(direction_x, direction_y)
+
+	move_and_slide()
+
+func play_animation(direction_x, direction_y):
+	
+	if (direction_y != 0):
+		if (direction_y == 1):
+			sprite.play("move_down")
+		else:
+			sprite.play("move_up")
+
+	elif (direction_x != 0):
+		if (direction_x == 1):
+			sprite.play("move_right")
+		else:
+			sprite.play("move_left")
+
+	else:
+		sprite.pause()
