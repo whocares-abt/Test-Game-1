ee1c33b2c4fe09fc6b78c4cd75c28ea6dc13deb1
whocares-abt <126311601+whocares-abt@users.noreply.github.com>
2025-12-21 02:31:05 +0530

    Updated NPCs to use dialogue interactables instead of implementing their own

--- Scripts/Characters/npc.gd
+++ Scripts/Characters/npc.gd
@@ -1,21 +1,9 @@
 extends Node2D
 
-@onready var interactable = $Interactable
+@onready var dialogue_interactable = $DialogueInteractable
 
-@export var npc_name = "Alice01"
 @export var dialogue_scene = load("res://Resources/Dialogues/test_dialogue.tres")
-
-@export var scene_id = 01
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	interactable.interact = _on_interact
-	SignalBus.connect("dialogue_finished", _on_dialogue_end)
-
-func _on_interact():
-	interactable.is_interactable = false
-	SignalBus.emit_signal("dialogue_started", dialogue_scene)
-
-func _on_dialogue_end(scene : DialogueScene):
-	if (scene.scene_id == scene_id):
-		interactable.is_interactable = true
+	dialogue_interactable.change_dialogue_scene(dialogue_scene)
